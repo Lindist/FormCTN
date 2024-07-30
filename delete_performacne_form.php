2c9491c904090b5131c207874e68c50eb1d15c8b
Lindist <four181049@gmail.com>
2024-07-30 09:38:24 +0700

    sucdel

--- delete_performacne_form.php
+++ delete_performacne_form.php
@@ -15,40 +15,10 @@ if (isset($_GET['id'])) {
     $result->execute();
     $row = $result->fetch(PDO::FETCH_ASSOC);
     $form_id = $row['form_id'];
-    
-    $in = $row['input_id'];
-    $pr = $row['process_id'];
-    $re = $row['report_id'];
-    $se = $row['senrity_id'];
-    
+
     $conn->query("DELETE FROM tb_efficiercy_form  WHERE form_id = '$id'")->execute(); 
 
-    $conn->query("DELETE FROM tb_input  WHERE Input_id = '$in'")->execute();
-    
-    $conn->query("DELETE FROM tb_process WHERE process_id = '$pr'")->execute();
-    
-    $conn->query("DELETE FROM tb_report WHERE report_id = '$re'")->execute();
-    
-    $conn->query("DELETE FROM tb_senrity WHERE senrity_id = '$se'")->execute();
-
-    $del_tb_fill_efficiercy = $conn->query("SELECT * FROM tb_fill_efficiercy WHERE form_id = '$form_id'");
-    $del_tb_fill_efficiercy->execute();
-    $row_tb_fill_efficiercy = $del_tb_fill_efficiercy->fetch(PDO::FETCH_ASSOC);
-    
-    $fill_in = $row_tb_fill_efficiercy['fill_input_id'];
-    $fill_pr = $row_tb_fill_efficiercy['fill_process_id'];
-    $fill_re = $row_tb_fill_efficiercy['fill_report_id'];
-    $fill_se = $row_tb_fill_efficiercy['fill_senrity_id '];
-
     $conn->query("DELETE FROM tb_fill_efficiercy  WHERE form_id = '$form_id'")->execute(); 
-
-    $conn->query("DELETE FROM tb_fill_input  WHERE fill_input_id = '$fill_in'")->execute();
-    
-    $conn->query("DELETE FROM tb_fill_process WHERE fill_process_id = '$fill_pr'")->execute();
-    
-    $conn->query("DELETE FROM tb_fill_report WHERE fill_report_id = '$fill_re'")->execute();
-    
-    $conn->query("DELETE FROM tb_fill_senrity WHERE fill_senrity_id = '$fill_se'")->execute();
 
     $_SESSION['delete'] = "ลบข้อมูลเรียบร้อย";
     header("location: form.php?class=columnData");
--- delete_satis_form.php
+++ delete_satis_form.php
@@ -15,41 +15,10 @@ if (isset($_GET['id'])) {
     $result->execute();
     $row = $result->fetch(PDO::FETCH_ASSOC);
     $sati_id = $row["sati_id"];
-
-    $ur = $row['ur_id'];
-    $fun = $row['fun_id'];
-    $uf = $row['uf_id'];
-    $ss = $row['ss_id'];
     
     $conn->query("DELETE FROM tb_satisfied  WHERE sati_id = '$id'")->execute(); 
 
-    $conn->query("DELETE FROM tb_user_req  WHERE ur_id = '$ur'")->execute();
-    
-    $conn->query("DELETE FROM tb_function WHERE fun_id = '$fun'")->execute();
-    
-    $conn->query("DELETE FROM tb_uesful WHERE uf_id = '$uf'")->execute();
-    
-    $conn->query("DELETE FROM tb_seurity WHERE ss_id = '$ss'")->execute(); 
-
-
-    $del_tb_fill_satisfied = $conn->query("SELECT * FROM tb_fill_satisfied WHERE sati_id = '$sati_id'");
-    $del_tb_fill_satisfied->execute();
-    $row_tb_fill_satisfied = $del_tb_fill_satisfied->fetch(PDO::FETCH_ASSOC);
-    
-    $fill_ur = $row_tb_fill_satisfied['fill_ur_id '];
-    $fill_fun = $row_tb_fill_satisfied['fill_fun_id'];
-    $fill_uf = $row_tb_fill_satisfied['fill_uf_id'];
-    $fill_ss = $row_tb_fill_satisfied['fill_ss_id'];
-
     $conn->query("DELETE FROM tb_fill_satisfied  WHERE sati_id = '$sati_id'")->execute(); 
-
-    $conn->query("DELETE FROM tb_fill_user_req  WHERE fill_ur_id = '$fill_ur'")->execute();
-    
-    $conn->query("DELETE FROM tb_fill_function WHERE fill_fun_id = '$fill_fun'")->execute();
-    
-    $conn->query("DELETE FROM tb_fill_uesful WHERE fill_uf_id = '$fill_uf'")->execute();
-    
-    $conn->query("DELETE FROM tb_fill_seurity WHERE fill_ss_id = '$fill_ss'")->execute();
 
     $_SESSION['delete'] = "ลบข้อมูลเรียบร้อย";
     header("location: form.php?class=columnData");
