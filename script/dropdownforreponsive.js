249a52b633043ddde8d62e0cb3686da9b9a639aa
Lindist <four181049@gmail.com>
2024-08-05 14:18:34 +0700

    finnally_serious3

---
+++ script/dropdownforreponsive.js
@@ -0,0 +1,16 @@
+const dropdown = document.querySelector('.responsive');
+const headerfordivide = document.querySelector('header'); 
+const seachmargin = document.querySelectorAll('.seachmargin');
+const marforbtnshow = document.querySelectorAll('.container > .btnshow');
+dropdown.addEventListener('click', () => {
+    document.querySelectorAll('.tab_box')[1].classList.toggle('active');
+    document.querySelectorAll('header .responsive .toggle-btn span').forEach(element => {
+        element.classList.toggle('spanactive');
+    });
+    marforbtnshow.forEach((e) =>{
+        e.style.marginTop = (headerfordivide.offsetHeight + 8) + "px";
+    });
+    seachmargin.forEach((e) =>{
+        e.style.marginTop = headerfordivide.offsetHeight + "px";
+    });
+});
