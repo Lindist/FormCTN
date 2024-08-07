4176ee9ddae80070bbfc5c254b5af3e6169e90b2
KwwClw <neskung50@gmail.com>
2024-08-07 10:17:34 +0700

    update pj_id fill

--- fill_satis.php
+++ fill_satis.php
@@ -14,6 +14,7 @@ if (isset($_POST['save'])) {
     $sati_id = $_POST['sati_id'];
     $sati_name = $_POST['sati_name'];
     $sati_comment = $_POST['sati_comment'];
+    $pj_id = $_POST['project_id'];
     
     $sub_info_ex = [];
     for ($i = 1; $i <= count($_POST['sati_info']); $i++) {
@@ -41,28 +42,6 @@ if (isset($_POST['save'])) {
     $ur_score = implode("ꓘ", $ur_score_ex);
     $sub_topic = implode("ꓘ", $combined_subtopic);
 
-    // echo $sati_id." ";
-    // echo $sati_name;
-    // echo "<br>";;
-    // echo "// ข้อมูลพื้นฐาน //";
-    // print_r($sati_info); // ข้อมูลพื้นฐาน
-    // echo "<br>";
-    // echo count($_POST['sati_info']);
-    // echo "<br>";
-    // echo "// ข้อมูลพื้นฐานย่อย //";
-    // print_r($sub_info); // ข้อมูลพื้นฐานย่อย
-    // echo "<br>";
-    // echo "// หัวข้อด้าน //";
-    // print_r($sati_topic); // หัวข้อด้าน
-    // echo "<br>";
-    // echo count($_POST['sati_topic']);
-    // echo "<br>";
-    // echo "// sub_topic //";
-    // print_r($sub_topic); // sub_topic
-    // echo "<br>";
-    // echo "// ur_score //";
-    // print_r($ur_score); // ur_score
-
 }
 
 function check_empty($array)
@@ -87,8 +66,8 @@ if (empty($sub_info)) {
     try {
         if (!isset($_SESSION['error'])) {
 
-            $tb_fill_satisfied = $conn->prepare("INSERT INTO tb_fill_satisfied (sati_id, sati_ep2, sati_comment, member_id, sati_info, sub_info, sati_topic, sub_topic, score) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $tb_fill_satisfied->execute([$sati_id, $sati_name, $sati_comment, $user_id, $sati_info, $sub_info, $sati_topic, $sub_topic, $ur_score]);
+            $tb_fill_satisfied = $conn->prepare("INSERT INTO tb_fill_satisfied (sati_id, sati_ep2, sati_comment, member_id, sati_info, sub_info, sati_topic, sub_topic, score, project_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $tb_fill_satisfied->execute([$sati_id, $sati_name, $sati_comment, $user_id, $sati_info, $sub_info, $sati_topic, $sub_topic, $ur_score, $project_id]);
 
             header("location: form.php");
 
