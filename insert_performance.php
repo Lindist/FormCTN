edd341275461771eeb12d45d71a6c39aa6434041
KwwClw <neskung50@gmail.com>
2024-08-07 14:56:22 +0700

    update

--- insert_performance.php
+++ insert_performance.php
@@ -53,29 +53,6 @@ if (isset($_POST['save'])) {
 
     $setfeature = implode("ꓘ", $combined_setfeature);
 
-    // $sub_info_ex = preg_split("/ꓘ/", $sub_info);
-    
-    // echo $formname;
-    // echo "<br>";
-    // echo $ad;
-    // echo "<br>";
-    // echo "// ข้อมูลพื้นฐาน //";
-    // echo $form_info; // ข้อมูลพื้นฐาน
-    // echo "<br>";
-    // echo count($_POST['form_info']);
-    // echo "// ข้อมูลพื้นฐานย่อย //";
-    // print_r($combined_subinfo); // ข้อมูลพื้นฐานย่อย
-    // echo "<br>";
-    // echo "// หัวข้อด้าน //";
-    // print_r($form_topic); // หัวข้อด้าน
-    // echo "<br>";
-    // echo count($_POST['form_topic']);
-    // echo "// คุณสมบัติด้านเทคนิค //";
-    // print_r($combined_feature); // คุณสมบัติด้านเทคนิค
-    // echo "<br>";
-    // echo "// คุณสมบัติที่ตั้งไว้ //";
-    // print_r($combined_setfeature); // คุณสมบัติที่ตั้งไว้
-
 }
 
 if (empty($formname)) {
@@ -102,8 +79,8 @@ if (empty($formname)) {
     try {
         if (!isset($_SESSION['error'])) {
 
-            $tb_efficiercy_form = $conn->prepare("INSERT INTO tb_efficiercy_form(form_name, form_ad, member_id, form_info, sub_info, form_topic, feature, setfeature,project_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $tb_efficiercy_form->execute([$formname, $ad, $user_id, $form_info, $sub_info, $form_topic, $feature, $setfeature,$projectId]);
+            $tb_efficiercy_form = $conn->prepare("INSERT INTO tb_efficiercy_form(form_name, form_ad, member_id, form_info, sub_info, form_topic, feature, setfeature, project_id) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $tb_efficiercy_form->execute([$formname, $ad, $user_id, $form_info, $sub_info, $form_topic, $feature, $setfeature, $projectId]);
             
             unset($_SESSION['projectId']);
             $_SESSION['save_form'] = "บึนทึก แบบฟอร์มประเมินประสิทธิภาพ เรียบร้อย";
--- showlistsatis.php
+++ showlistsatis.php
@@ -40,7 +40,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
     <link rel="icon" type="icon" href="https://i.imgur.com/m0H7jcN.png">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <title>Document</title>
+    <title>รายชื่อผู้กรอก</title>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
 
