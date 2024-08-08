411501a072290854474425eecaf08fc830314b90
KwwClw <neskung50@gmail.com>
2024-08-08 20:11:07 +0700

    update dowload qr code

--- showlistsatis.php
+++ showlistsatis.php
@@ -1,39 +1,40 @@
 <?php
-    session_start();
-    require 'session/config.php';
-    
-    if (!isset($_SESSION['user_id'])) {
-        header("Location: index.php");
-    } else if (isset($_SESSION['user_id'])) {
-        $user_id = $_SESSION['user_id'];
-    }
-    if(isset($_GET['id'])){
-        $id = $_GET['id'];
-    }
-    $result = $conn->query("SELECT * FROM tb_member WHERE member_id = '$user_id'");
-    $count = count($result->fetchAll());
-    $result->execute();
+session_start();
+require 'session/config.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php");
+} else if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+}
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+$result = $conn->query("SELECT * FROM tb_member WHERE member_id = '$user_id'");
+$count = count($result->fetchAll());
+$result->execute();
 
 
-    $tb_fill_satisfied = $conn->query("SELECT * FROM tb_fill_satisfied WHERE sati_id = '$id'");
-    $tb_fill_satisfied->execute();
+$tb_fill_satisfied = $conn->query("SELECT * FROM tb_fill_satisfied WHERE sati_id = '$id'");
+$tb_fill_satisfied->execute();
 
-    $nofiller = array();
-    $filler = array();
-        while($row_tb_fill_satisfied = $tb_fill_satisfied->fetch(PDO::FETCH_ASSOC)){
-            $nofiller[] = $row_tb_fill_satisfied["member_id"];
-        }
-        for($i = 0; $i < count($nofiller);$i++){
-            $member_id = $conn->query("SELECT * FROM tb_member WHERE member_id = '$nofiller[$i]'");
-            $member_id->execute();
-            $row = $member_id->fetch(PDO::FETCH_ASSOC);
-            $filler[] = $row;
-        }
-    
+$nofiller = array();
+$filler = array();
+while ($row_tb_fill_satisfied = $tb_fill_satisfied->fetch(PDO::FETCH_ASSOC)) {
+    $nofiller[] = $row_tb_fill_satisfied["member_id"];
+}
+for ($i = 0; $i < count($nofiller); $i++) {
+    $member_id = $conn->query("SELECT * FROM tb_member WHERE member_id = '$nofiller[$i]'");
+    $member_id->execute();
+    $row = $member_id->fetch(PDO::FETCH_ASSOC);
+    $filler[] = $row;
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -41,6 +42,7 @@
     <link rel="icon" type="icon" href="https://i.imgur.com/m0H7jcN.png">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>รายชื่อผู้กรอก</title>
+    <script src="https://unpkg.com/qr-code-styling/lib/qr-code-styling.js"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap');
 
@@ -51,24 +53,27 @@
             background-color: rgb(180, 180, 180);
             color: #000;
         }
-        body > div {
+
+        body>div {
             height: 95vh;
         }
-        body > div #form{
+
+        body>div #form {
             width: 100%;
             padding: 1rem;
             height: 100%;
             overflow-y: scroll;
         }
 
-        main{
+        main {
             display: grid;
             grid-template-columns: 1fr;
             grid-row-gap: 10px;
             padding: 0 2rem;
             width: 100%;
         }
-        main > div{
+
+        main>div {
             display: flex;
             justify-content: space-between;
             align-items: center;
@@ -82,44 +87,51 @@
             border-bottom-style: solid;
             border-bottom-color: rgba(88, 88, 88, 0.685);
         }
-        .text1,i{
+
+        .text1,
+        i {
             font-size: 25px;
             display: flex;
             justify-content: center;
             align-items: center;
         }
-        .imgage{
+
+        .imgage {
             position: absolute;
-            left: 50%; 
-            top: 70%; 
-            transform: translate(-50%,-70%);
+            left: 50%;
+            top: 70%;
+            transform: translate(-50%, -70%);
         }
+
         @media all and (max-width:800px) {
             body {
-            font-family: "Kanit", sans-serif;
-            font-weight: 500;
-            font-style: normal;
-            background-color: rgb(180, 180, 180);
-            color: #000;
+                font-family: "Kanit", sans-serif;
+                font-weight: 500;
+                font-style: normal;
+                background-color: rgb(180, 180, 180);
+                color: #000;
             }
-            body > div {
+
+            body>div {
                 height: 95vh;
             }
-            body > div #form{
+
+            body>div #form {
                 width: 100%;
                 padding: 0;
                 height: 100%;
                 overflow-y: scroll;
             }
 
-            main{
+            main {
                 display: grid;
                 grid-template-columns: 1fr;
                 grid-row-gap: 10px;
                 padding: 0;
                 width: 100%;
             }
-            main > div{
+
+            main>div {
                 place-self: center;
                 display: flex;
                 text-align: center;
@@ -136,18 +148,22 @@
                 border-bottom-style: solid;
                 border-bottom-color: rgba(88, 88, 88, 0.685);
             }
-            .text1,i{
-            font-size: 16px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-        .imgage{
-            display: none;
+
+            .text1,
+            i {
+                font-size: 16px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .imgage {
+                display: none;
             }
         }
     </style>
 </head>
+
 <body>
     <div class="main container-fluid col-11 bg-white py-1 my-3 rounded">
         <form action="" id="form">
@@ -156,20 +172,27 @@
             </button>
             <h1 class="header text-center my-5 text-break">รายชื่อผู้กรอกและผู้สร้างฟอร์ม</h1>
             <main>
-                    
+
             </main>
         </form>
     </div>
     <script type="text/javascript">
         const showform = document.querySelector('main');
-            showform.insertAdjacentHTML("beforeend", `
+        showform.insertAdjacentHTML("beforeend", `
             <?php for ($c = 1; $c <= $count; $c++) { ?>
                 <?php $row = $result->fetch(PDO::FETCH_ASSOC); ?>
                     <span>
                     <!-- <div>ลำดับที่ <?php echo $c; ?></div> -->
-                    <div class='text1'><i class='bx bxs-user-circle'></i><?php echo $row['member_title']." ".$row['member_firstname']." ".$row['member_lastname']." ".$row['member_code']." "."(ผู้สร้าง)"; ?>
-                    <?php if(count($nofiller) > 0){ ?>
+                    <div class='text1'><i class='bx bxs-user-circle'></i><?php echo $row['member_title'] . " " . $row['member_firstname'] . " " . $row['member_lastname'] . " " . $row['member_code'] . " " . "(ผู้สร้าง)"; ?>
+                    <?php if (count($nofiller) > 0) { ?>
                     <a id='btnresult' href="sum_sati.php?sati_id=<?= $id; ?>" >ผลสรุป</a>
+
+                    <div class="">
+                        <button onclick="download_btn(<?= $id; ?>)" class="">
+                            Download QR Code
+                        </button>
+                    </div>
+
                     <?php } ?>
                     </div>
                     <!-- <div class="btns">
@@ -177,30 +200,30 @@
                     </div> -->
                     </span>
                 <?php } ?>
-                <?php if(count($nofiller) > 0){ ?>
+                <?php if (count($nofiller) > 0) { ?>
                     <?php for ($i = 0; $i < count($nofiller); $i++) { ?>
                             <div>
-                            <div>ลำดับที่ <?php echo $i+1; ?></div>
-                            <div class='text'><?php echo $filler[$i]['member_title']." ".$filler[$i]['member_firstname']." ".$filler[$i]['member_lastname']." ".$filler[$i]['member_code']; ?></div>
+                            <div>ลำดับที่ <?php echo $i + 1; ?></div>
+                            <div class='text'><?php echo $filler[$i]['member_title'] . " " . $filler[$i]['member_firstname'] . " " . $filler[$i]['member_lastname'] . " " . $filler[$i]['member_code']; ?></div>
                             <div class="btns">
                             <a id='btn' href="showfilledstatisfied.php?user_id=<?php echo $filler[$i]['member_id']; ?>&form_id=<?= $id; ?>" >ดูข้อมูล</a>
                             <!-- <a id='btnresult' href="resultstatisfield.php?user_id=<?php echo $filler[$i]['member_id']; ?>&form_id=<?= $id; ?>" >ผลสรุป</a> -->
                             </div>
                             </div>
                     <?php } ?>
-                <?php }else{ ?>
+                <?php } else { ?>
                     <span class="imgage"">
                         <img src="picture/form.png" style="width: 10rem; position: relative; left: 50%; transform: translateX(-50%)">
                         <h3 style="text-align: center;">ไม่มีการกรอกข้อมูลแบบสอบถาม</h3>
                     </span>
                 <?php } ?>
                 `);
-        backtoindex = () =>{
+        backtoindex = () => {
             window.location.href = 'form.php';
         };
     </script>
     <style>
-        .btns > a{
+        .btns>a {
             margin: 0;
             display: inline-block;
             text-align: center;
@@ -215,10 +238,12 @@
             transition: all .2s ease-in-out;
 
         }
-        .btns > a:hover{
+
+        .btns>a:hover {
             background-color: rgb(3, 3, 159);
         }
-        #btnresult{
+
+        #btnresult {
             background-color: #088395;
             font-size: 19px;
             margin-left: 5px;
@@ -232,51 +257,87 @@
             color: #fff;
             transition: all .2s ease-in-out;
         }
-        #btnresult:hover{
+
+        #btnresult:hover {
             background-color: #37B7C3;
         }
+
         @media all and (max-width:800px) {
-            #btnresult{
-            background-color: #088395;
-            font-size: 16px;
-            margin-left: 5px;
-            display: inline-block;
-            text-align: center;
-            text-decoration: none;
-            padding: 0.2rem 0.5rem;
-            border-radius: 5px;
-            box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
-            cursor: pointer;
-            color: #fff;
-            transition: all .2s ease-in-out;
-            font-weight: 100;
+            #btnresult {
+                background-color: #088395;
+                font-size: 16px;
+                margin-left: 5px;
+                display: inline-block;
+                text-align: center;
+                text-decoration: none;
+                padding: 0.2rem 0.5rem;
+                border-radius: 5px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
+                cursor: pointer;
+                color: #fff;
+                transition: all .2s ease-in-out;
+                font-weight: 100;
             }
-            #btnresult:hover{
+
+            #btnresult:hover {
                 background-color: #37B7C3;
             }
-            .btns > a{
-            margin: 0;
-            font-size: 16px;
-            display: inline-block;
-            text-align: center;
-            text-decoration: none;
-            width: 100px;
-            padding: 0.3rem;
-            background-color: #00f;
-            border-radius: 5px;
-            box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
-            cursor: pointer;
-            color: #fff;
-            transition: all .2s ease-in-out;
+
+            .btns>a {
+                margin: 0;
+                font-size: 16px;
+                display: inline-block;
+                text-align: center;
+                text-decoration: none;
+                width: 100px;
+                padding: 0.3rem;
+                background-color: #00f;
+                border-radius: 5px;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.5);
+                cursor: pointer;
+                color: #fff;
+                transition: all .2s ease-in-out;
 
             }
-            .btns > a:hover{
+
+            .btns>a:hover {
                 background-color: rgb(3, 3, 159);
             }
-            .text{
+
+            .text {
                 font-size: 16px;
             }
         }
     </style>
+
+    <script>
+        download_btn = (index) => {
+            let qrCode = new QRCodeStyling({
+                width: 300,
+                height: 300,
+                data: `http://localhost/FormCTN/fill_satis_form.php?id=${index}`,
+                dotsOptions: {
+                    color: "#000000",
+                },
+                backgroundOptions: {
+                    color: "#ffffff",
+                },
+                imageOptions: {
+                    crossOrigin: "anonymous",
+                    margin: 20
+                }
+            });
+
+            qrCode.download({
+                name: "qr-code",
+                extension: "png"
+            }).then(() => {
+                // หลังจากการดาวน์โหลดเสร็จสมบูรณ์ ให้กลับมายังหน้าที่ต้องการ
+                window.location.href = `http://localhost/FormCTN/showlistsatis.php?id=${index}`;
+            });
+        };
+    </script>
+
 </body>
+
 </html>
